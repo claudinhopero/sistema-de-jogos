211c56fff920d8f69d30137420c90271fb1cb5cb
Cláudio César Gonçalves de Faria Filho <claudinhopero@gmail.com>
2022-09-10 00:48:36 -0300

    Retornando um commit

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 //outra mensagem de log
 console.log("modificação entre aulas")
-console.log("enviando para o github")
